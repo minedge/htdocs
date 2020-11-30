9f58e4ebf155f9fc12d99127cd71f137550f0e07
mhlee <minedge2@gmail.com>
2020-11-30 20:04:02 +0900

    SDMZ 설정 완료
    가즈아

--- apmtest.php
+++ apmtest.php
@@ -1,15 +1,22 @@
 <?php
-    echo "Before DB Connection\n";
     $db_host="localhost";   
     $db_user="root";
     $db_passwd="1324"; 
     $db_name="bucketlist";
 
+    $id = $_POST['ID'];
+
     $conn = mysqli_connect($db_host, $db_user, $db_passwd, $db_name);
-    $sql = "SELECT * FROM minedge";
+    $sql = "SELECT * FROM $id";
     $result = mysqli_query($conn, $sql);
     $row = mysqli_fetch_array($result);
 
-    echo "After DB Connection\n";
     echo $row[0];
+    echo '|';
+    echo $row[1];
+    echo '|';
+    echo $row[2];
+    echo '|';
+    echo $row[3];
+    echo '|';
 ?>
