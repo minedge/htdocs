323f1d3e2466b961f4c7335d92e0de1604dff598
parkjooyeon <sb03063@naver.com>
2020-12-03 00:24:18 +0900

    Create profile.php

---
+++ profile.php
@@ -0,0 +1,17 @@
+<?php
+    $db_host="localhost";   
+    $db_user="root";
+    $db_passwd="1324"; 
+    $db_name="bucketlist";
+
+    $id = $_POST['ID'];
+
+    select name from userinfo where id='$id';
+
+    $conn = mysqli_connect($db_host, $db_user, $db_passwd, $db_name);
+    $sql = "SELECT name FROM userinfo WHERE id='$id'";
+    $result = mysqli_query($conn, $sql);
+    $row = mysqli_fetch_array($result);
+
+    echo $row[0];
+?>
