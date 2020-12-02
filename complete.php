f20b7f69cc80f663ff47dd470841da83cd45169c
parkjooyeon <sb03063@naver.com>
2020-12-03 01:16:27 +0900

    1이래ㅋ

---
+++ complete.php
@@ -0,0 +1,14 @@
+<?php
+    $db_host="localhost";   
+    $db_user="root";
+    $db_passwd="1324"; 
+    $db_name="bucketlist";
+
+    $id = $_POST['ID'];
+
+    $conn = mysqli_connect($db_host, $db_user, $db_passwd, $db_name);
+    $sql = "SELECT COUNT(title) FROM $id WHERE complete='1'";
+    $result = mysqli_query($conn, $sql);
+    $row = mysqli_fetch_array($result);
+
+    echo $row[0];
