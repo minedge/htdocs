10ddf62d1c6dfa7bf11a566ec5c4919fc704494d
mhlee <minedge2@gmail.com>
2020-12-03 15:46:25 +0900

    기능 추가
    검색 및 정렬 기능 추가

---
+++ search.php
@@ -0,0 +1,26 @@
+<?php
+    $db_host="localhost";   
+    $db_user="root";
+    $db_passwd="1324"; 
+    $db_name="bucketlist";
+
+    $id = $_POST['ID'];
+    $target = $_POST['TARGET'];
+
+    $conn = mysqli_connect($db_host, $db_user, $db_passwd, $db_name);
+    $sql = "SELECT * FROM $id WHERE title LIKE '%$target%'";
+    $result = mysqli_query($conn, $sql);
+    
+    while ($row = mysqli_fetch_array($result)){
+        echo $row[0];
+        echo '@';
+        echo $row[1];
+        echo '@';
+        echo $row[2];
+        echo '@';
+        echo $row[3];
+        echo '@';
+        echo $row[4];
+        echo '@';
+     }
+?>
---
+++ sort.php
@@ -0,0 +1,32 @@
+<?php
+    $db_host="localhost";   
+    $db_user="root";
+    $db_passwd="1324"; 
+    $db_name="bucketlist";
+
+    $id = $_POST['ID'];
+    $target = $_POST['TARGET'];
+    $type = $_POST['TYPE'];
+
+
+    $conn = mysqli_connect($db_host, $db_user, $db_passwd, $db_name);
+    if($type == "1"){
+        $sql = "SELECT * FROM $id ORDER BY $target asc";
+    }else{
+        $sql = "SELECT * FROM $id ORDER BY $target desc";
+    }
+    $result = mysqli_query($conn, $sql);
+    
+    while ($row = mysqli_fetch_array($result)){
+        echo $row[0];
+        echo '@';
+        echo $row[1];
+        echo '@';
+        echo $row[2];
+        echo '@';
+        echo $row[3];
+        echo '@';
+        echo $row[4];
+        echo '@';
+     }
+?>
