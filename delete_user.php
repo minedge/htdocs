6d4a50709e9b45b7ac5e965a5d76b4548bec5e1b
parkjooyeon <sb03063@naver.com>
2020-12-04 22:35:10 +0900

    Create delete_user.php

---
+++ delete_user.php
@@ -0,0 +1,12 @@
+<?php
+    $db_host="localhost";   
+    $db_user="root";
+    $db_passwd="1324"; 
+    $db_name="bucketlist";
+
+    $id = $_POST['ID'];
+
+    $conn = mysqli_connect($db_host, $db_user, $db_passwd, $db_name);
+    $sql = "DROP TABLE $ID";
+    $result = mysqli_query($conn, $sql);
+?>
