21b04a4926083d2837c2484cc25ea506cd95f7e9
parkjooyeon <sb03063@naver.com>
2020-12-05 01:17:51 +0900

    delete_user.php 수정

--- delete_user.php
+++ delete_user.php
@@ -9,4 +9,6 @@
     $conn = mysqli_connect($db_host, $db_user, $db_passwd, $db_name);
     $sql = "DROP TABLE $id";
     $result = mysqli_query($conn, $sql);
+    $sql = "DELETE FROM userinfo WHERE name='$id'";
+    $result = mysqli_query($conn, $sql);
 ?>
