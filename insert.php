eed7190872ce51406ea9b2935360a7a9e669bb5c
mhlee <minedge2@gmail.com>
2020-12-04 22:32:04 +0900

    +a

--- insert.php
+++ insert.php
@@ -3,6 +3,7 @@
     $db_user="root";
     $db_passwd="1324"; 
     $db_name="bucketlist";
+    $conn = mysqli_connect($db_host, $db_user, $db_passwd, $db_name);
 
     $id = $_POST['ID'];
     $title = $_POST['TITEL'];
@@ -10,15 +11,26 @@
     $complete = $_POST['COMPLT'];
     $tdate = $_POST['TDATE'];
     $body = $_POST['BODY'];
-    $image = $_POST['IMAGE'];
     $ident_num = $_POST['IDNT_NUM'];
 
-    $conn = mysqli_connect($db_host, $db_user, $db_passwd, $db_name);
-    if($ident_num == "0"){
-        $sql = "INSERT INTO $id VALUES (null, '$title', '$local', '$complete', '$tdate', '$body', '$image')";
+    $image = $_POST['IMAGE'];
+    if($image != ""){
+        $data=base64_decode($image);
+        $escaped_values=mysql_real_escape_string($data);
+        
+        if($ident_num == "0"){
+            $sql = "INSERT INTO $id VALUES (null, '$title', '$local', '$complete', '$tdate', '$body', '$escaped_values')";
+        }else{
+            $sql = "UPDATE $id SET title='$title', locate='$local', complete='$complete', target_date='$tdate', body='$body', image='$escaped_values' WHERE ident='$ident_num'";
+        }
     }else{
-        $sql = "UPDATE $id SET title='$title', locate='$local', complete='$complete', target_date='$tdate', body='$body', image='$image' WHERE ident='$ident_num'";
+        if($ident_num == "0"){
+            $sql = "INSERT INTO $id VALUES (null, '$title', '$local', '$complete', '$tdate', '$body', '$escaped_values')";
+        }else{
+            $sql = "UPDATE $id SET title='$title', locate='$local', complete='$complete', target_date='$tdate', body='$body', image='$escaped_values' WHERE ident='$ident_num'";
+        }
     }
+
     $result = mysqli_query($conn, $sql);
 
     mysql_close($conn);
