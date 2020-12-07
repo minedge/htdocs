5d38a32d8e563dd5a18d5a3503e0e0df40c5cd0e
mhlee <minedge2@gmail.com>
2020-12-07 19:46:17 +0900

    오류 수정
    오이오이 이름=아이디 하면 지워지겠냐능~~

--- delete_user.php
+++ delete_user.php
@@ -9,6 +9,6 @@
     $conn = mysqli_connect($db_host, $db_user, $db_passwd, $db_name);
     $sql = "DROP TABLE $id";
     $result = mysqli_query($conn, $sql);
-    $sql = "DELETE FROM userinfo WHERE name='$id'";
+    $sql = "DELETE FROM userinfo WHERE id='$id'";
     $result = mysqli_query($conn, $sql);
 ?>
