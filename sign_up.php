c1060db4821ebb87fe6598a99c544f0cf7be5fed
mhlee <minedge2@gmail.com>
2020-12-08 15:26:16 +0900

    왜 default null 이지??
    not null로 했던거 같은데....;;

--- sign_up.php
+++ sign_up.php
@@ -11,6 +11,6 @@
     $conn = mysqli_connect($db_host, $db_user, $db_passwd, $db_name);
     $sql = "INSERT INTO userinfo VALUES (null,'$id','$pw','$name')";
     $result = mysqli_query($conn, $sql);
-    $sql = "CREATE TABLE $id( ident int not null AUTO_INCREMENT, title varchar(20) default null, locate int default null, complete int default null, target_date date default null, body varchar(100) default null, image LONGBLOB not null, primary key(ident) )default charset=utf8";
+    $sql = "CREATE TABLE $id( ident int not null AUTO_INCREMENT, title varchar(20) not null, locate int not null, complete int not null, target_date date not null, body varchar(100) not null, image LONGBLOB not null, primary key(ident) )default charset=utf8";
     $result = mysqli_query($conn, $sql);
 ?>
